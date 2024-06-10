c43173f45865809cbb880caba967d4358b49e413
sixteenis <psm0103907@naver.com>
2024-06-10 16:09:02 +0900

    Initial Commit

---
+++ MediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaProject
+//
+//  Created by 박성민 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
